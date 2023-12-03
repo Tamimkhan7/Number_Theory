b59e7b0bfba7c7e564524a49ff1b70f3ebbaaa89
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-12-03 22:32:48 +0600

    second commit

--- Prime Factorization.cpp
+++ Prime Factorization.cpp
@@ -1,31 +1,27 @@
-//bujte hobe pore
 #include <bits/stdc++.h>
 using namespace std;
-#define n1     '\n'
+#define n1 '\n'
 int main()
 {
     int t;
     int n;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        cin>>n;
-        //cout<<endl;
-        for(int i=2; i*i<=n; i++)
+        cin >> n;
+        for (int i = 2; i <= sqrt(n); i++)
         {
-            if(n%i==0)
+            if (n % i == 0)
             {
-                int c=0;
-                while(n%i==0)
+                while (n % i == 0)
                 {
-                    c++;
-                    n=n/i;
+                    n = n / i;
+                    cout << i << " ";
                 }
-                cout<<i<<" ^ "<<c<<" , ";
             }
         }
-        if(n>1)cout<<n<<" ^ "<<1<<endl;
-        else cout<<endl;
+        if (n > 1)
+            cout << n;
+        cout << endl;
     }
-
 }
